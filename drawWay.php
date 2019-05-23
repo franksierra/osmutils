405fd51e94c6c7c11f93f4a944a66e6cb007a7f2
Frank Sierra <sierrafayad@gmail.com>
2019-05-23 02:57:22 -0500

    This is mostly for debugging purposes

--- drawWay.php
+++ drawWay.php
@@ -6,12 +6,11 @@ include __DIR__ . "/class/Extractor.php";
 $extractor = new Extractor();
 
 $box = $extractor->get_box(108089)[0];
-$poly = $extractor->run(108089);
-$polyfull = $extractor->runfull();
-
+$extract = $extractor->run(108089);
 header("Content-type: image/png");
 
-$scale = 500;
+$scale = 700;
+
 $img_width = distance($box["maxLAT"], $box["minLON"], $box["minLAT"], $box["maxLON"]) * ($scale / 100);
 $img_height = distance($box["maxLAT"], $box["minLON"], $box["minLAT"], $box["minLON"]) * ($scale / 100);
 
@@ -22,29 +21,43 @@ $red = imagecolorallocate($img, 255, 0, 0);
 $blue = imagecolorallocate($img, 0, 0, 255);
 imagefill($img, 0, 0, $white);
 
-$point = $poly[0];
-unset($poly[0]);
+
+$point = $extract['points'][0];
+unset($extract['points'][0]);
 $P = point($box, $point['lat'], $point['lon'], $scale, $img_height);
+$firstPx = $P['x'];
+$firstPy = $P['y'];
 
-foreach ($poly as $point) {
+$color = rand_color($img);
+$color = $way_id = $point["way_id"];
+foreach ($extract['points'] as $point) {
     $Pn = point($box, $point['lat'], $point['lon'], $scale, $img_height);
-
-//    imageline($img, $P['x'], $P['y'], $Pn['x'], $Pn['y'], $red);
-    imagesetpixel($img, $Pn['x'], $Pn['y'], $blue);
+    if ($way_id != $point["way_id"]) {
+        $color = rand_color($img);
+        $color = $way_id;
+        $way_id = $point["way_id"];
+        if (isset($extract['open_ways'][$way_id])) {
+            imagestring($img, 1, $P['x'], $P['y'], $way_id, $color);
+        }
+    }
+    imageline($img, $P['x'], $P['y'], $Pn['x'], $Pn['y'], $color);
     $P = $Pn;
 }
 
-$point = $polyfull[0];
-unset($polyfull[0]);
-$P = point($box, $point['lat'], $point['lon'], $scale, $img_height);
 
-foreach ($polyfull as $point) {
-    $Pn = point($box, $point['lat'], $point['lon'], $scale, $img_height);
-
-    imageline($img, $P['x'], $P['y'], $Pn['x'], $Pn['y'], $red);
+//$point = $polyfull[0];
+//unset($polyfull[0]);
+//$P = point($box, $point['lat'], $point['lon'], $scale, $img_height);
+//
+//foreach ($polyfull as $point) {
+//    $Pn = point($box, $point['lat'], $point['lon'], $scale, $img_height);
+//
+////    imageline($img, $P['x'], $P['y'], $Pn['x'], $Pn['y'], $red);
 //    imagesetpixel($img, $Pn['x'], $Pn['y'], $blue);
-    $P = $Pn;
-}
+//    $P = $Pn;
+//}
+
+
 imagepng($img);
 
 function distance($lat1, $lon1, $lat2, $lon2)
@@ -71,4 +84,9 @@ function point($boundaries, $lat, $lon, $scale = 100, $flip = 0)
         "x" => $x,
         "y" => $y
     ];
+}
+
+function rand_color($img)
+{
+    return imagecolorallocate($img, rand(0, 255), rand(0, 255), rand(0, 255));
 }
